0cced33a19b29b5388a6c250016c8dde05f8f930
bdcarvalho <bdcarvalho0798@gmail.com>
2024-10-02 22:31:30 +0900

    add changes

--- myApp.js
+++ myApp.js
@@ -5,14 +5,30 @@ let app = express();
 
 
 
+require('dotenv').config(); 
 
 
+// app.use(express.static(__dirname + "/public"));
+
+// // Assets at the /public route
+// app.use("/public", express.static(__dirname + "/public"));
 
 
+// app.get('/json', (req, res) => {
+//     res.json({ message: "Hello json" });
+//   });
 
 
-
-
+  app.get('/json', (req, res) => {
+    var message ="";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = "Hello json".toUpperCase();
+    }else{
+        message = "Hello json";
+    }
+    res.json({ message: message});
+  });
+  
 
 
 
